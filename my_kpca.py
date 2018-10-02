63f7a68ea50ec16cc15cba05350c307519b4164c
Muaz <muaz.sy123@gmail.com>
2018-10-02 16:08:46 +0200

    Add new dataset, add new kernel

--- my_kpca.py
+++ my_kpca.py
@@ -4,7 +4,8 @@
 
 import matplotlib.pyplot as plt
 
-from numpy import ones, exp, loadtxt
+from sklearn.datasets import make_moons
+from numpy import ones, exp, loadtxt, tanh
 from numpy.linalg import eig, norm
 
 VERBOSE = False
@@ -12,13 +13,16 @@ def __DEBUG(msg):
 	if VERBOSE: print(msg)
 
 K2_SIGMA = 0.007
-
+K3_alpha = 2
+K3_constatnt = 1
 def polynomial_kernel(X, Y):
 	return (X.T.dot(Y) + 1) ** 2
 
 def gaussian_kernel(X, Y):
-	return exp((-1 * (norm(X - Y) ** 2)) / (2 * (K2_SIGMA ** 2)))
+	return exp( (-1 * (norm(X - Y) ** 2)) / (2 * (K2_SIGMA ** 2)) )
 
+def sigmoid_kernel(X, Y):
+	return tanh(K3_alpha*X.T.dot(Y) + K3_constatnt)
 def k_matrix(A, kernel):
 
 	n = A.shape[0]
@@ -42,8 +46,6 @@ def k_matrix(A, kernel):
 	return K_
 	
 def kpca(A, kernel, Y):
-
-	
 
 	n = A.shape[0]
 	d = A.shape[1]
@@ -75,28 +77,54 @@ def kpca(A, kernel, Y):
 			A_new[i,j] = coco
 			
 	#__DEBUG("A_new = \n" + str(A_new))
-
-	plt.figure(1, figsize=(8, 6))
-	plt.clf()
-	plt.scatter(A[:, 0], A[:, 1], c=Y,s=25, edgecolor='k')
-    
-	plt.figure(2, figsize=(8, 6))
-	plt.clf()
-	plt.scatter(A_new[:, 0], A_new[:, 1], c=Y,s=25, edgecolor='k')
-
 	return A_new
 
 	
 if __name__ == "__main__":
 	from numpy import array
-	VERBOSE = True
-
-	data = loadtxt("C:\\Users\\Muaz\\Desktop\\Advanced_ML_practical_M2_MLDM\\data.data")
-	#__DEBUG("data : \n" + str(data))
-	d = data.shape[1]
-	A = data[:,0:(d-1)]
-	Y = data[:,(d-1):d].T[0]
+	#VERBOSE = True
+	readfile = False
+	if readfile:
+		data = loadtxt("C:\\Users\\Muaz\\Desktop\\Advanced_ML_practical_M2_MLDM\\data.data")
+		#__DEBUG("data : \n" + str(data))
+		d = data.shape[1]
+		A = data[:,0:(d-1)]
+		Y = data[:,(d-1):d].T[0]
+	else:
+		A, Y = make_moons(n_samples=100, noise=0.1)
+        
+	plt.figure(1, figsize=(8, 6))
+	plt.clf()
+	plt.scatter(A[:, 0], A[:, 1], c=Y,s=25, edgecolor='k')
 	#__DEBUG("A : \n" + str(A))
 	#__DEBUG("Y : \n" + str(Y))
-	kpca(A, polynomial_kernel,Y)
+
+	A_new = kpca(A, polynomial_kernel,Y)
+	plt.figure(2, figsize=(8, 6))
+	plt.clf()
+	plt.scatter(A_new[:, 0], A_new[:, 1], c=Y,s=25, edgecolor='k')
+
+	alpha_array = [0.001,0.01,1,100]
+	constant_array = [0.001,0.01,1,100]
 	
+	sigma_array = [0.0001,0.001,0.01,0.1,1,10,100]
+	cc = 3
+	for sigma in sigma_array:
+		        
+		K2_SIGMA = sigma
+		A_new = kpca(A, gaussian_kernel,Y)
+		plt.figure(cc, figsize=(8, 6))
+		plt.clf()
+		plt.scatter(A_new[:, 0], A_new[:, 1], c=Y,s=25, edgecolor='k')
+		cc = cc + 1
+		
+	for alpha in alpha_array:
+		for c in constant_array:
+			K3_alpha = alpha
+			K3_constatnt = c
+			A_new = kpca(A, sigmoid_kernel,Y)
+			plt.figure(cc, figsize=(8, 6))
+			plt.clf()
+			plt.scatter(A_new[:, 0], A_new[:, 1], c=Y,s=25, edgecolor='k')
+			cc = cc + 1
+    
