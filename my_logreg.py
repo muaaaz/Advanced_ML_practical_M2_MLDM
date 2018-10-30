a35b1f78830373e1117cf03c310fd1e093bb244a
Aboud <aboud.zakaria@gmail.com>
2018-10-30 14:24:44 +0100

    kernel as parameter, implement stochastic gradient descent

--- my_logreg.py
+++ my_logreg.py
@@ -36,25 +36,31 @@ def predict(X, theta, threshold=0.5):
 def rbf_kernel(v1, v2, sigma=1.0):
     return np.exp((-1 * np.linalg.norm(v1 - v2) ** 2) / (2 * sigma ** 2))
 
-def compute_kmat(X):
+def polynomial_kernel(v1, v2, c=1, d=2):
+    return (v1.T.dot(v2) + c) ** d
+
+def sigmoid_kernel(v1, v2, alpha=2, c=1):
+    return np.tanh(alpha * v1.T.dot(v2) + c)
+
+def compute_kmat(X, kernel):
     m = X.shape[0]
     kmat = np.zeros((m, m))
     for i in range(m):
         for j in range(m//2 + 1):
-            kmat[i, j] = kmat[j, i] = rbf_kernel(X[i], X[j])
+            kmat[i, j] = kmat[j, i] = kernel(X[i], X[j])
     return kmat
 
-def compute_kmat2(X, train):
+def compute_kmat2(X, train, kernel):
     m = X.shape[0]
     d = train.shape[0]
     kmat = np.zeros((m, d))
     for i in range(m):
         for j in range(d):
-            kmat[i, j] = rbf_kernel(X[i], train[j])
+            kmat[i, j] = kernel(X[i], train[j])
     return kmat
 
-def k_fit(X, y, learn_rate=0.01, num_iter=100000):
-    kmat = compute_kmat(X)
+def k_fit(X, y, learn_rate=0.01, num_iter=100000, kernel=rbf_kernel):
+    kmat = compute_kmat(X, kernel)
     kmat = add_ones_feature(kmat)
     m = kmat.shape[0]
     d = kmat.shape[1]
@@ -70,8 +76,29 @@ def k_fit(X, y, learn_rate=0.01, num_iter=100000):
 
     return theta
 
-def k_predict(X, train, theta, threshold=0.5):
-    kmat = compute_kmat2(X, train)
+def k_fit_stochastic(X, y, learn_rate=0.01, num_iter=10000, kernel=rbf_kernel):
+    kmat = compute_kmat(X, kernel)
+    kmat = add_ones_feature(kmat)
+    m = kmat.shape[0]
+    d = kmat.shape[1]
+    theta = np.zeros(d)
+    for iteration in range(num_iter):
+        for i in range(m):
+            h = sigmoid(np.dot(kmat, theta))
+            gradient = np.dot(kmat[i].T, (h[i] - y[i]))
+            theta -= learn_rate * gradient
+
+        if iteration % (num_iter // 10) == 0:
+            print(f'loss: {loss(h, y)}')
+
+    return theta
+
+
+
+
+
+def k_predict(X, train, theta, threshold=0.5, kernel=rbf_kernel):
+    kmat = compute_kmat2(X, train, kernel)
     kmat = add_ones_feature(kmat)
     return sigmoid(np.dot(kmat, theta)) >= threshold
 
@@ -90,6 +117,8 @@ if __name__ == '__main__':
 
     theta = k_fit(X_train, y_train)
     p = k_predict(X_test, X_train, theta)
-    
     print(f'accuracy: {int((p == y_test).mean() * 100)}%')
 
+    theta = k_fit_stochastic(X_train, y_train, num_iter=5000)
+    p = k_predict(X_test, X_train, theta)
+    print(f'accuracy: {int((p == y_test).mean() * 100)}%')
